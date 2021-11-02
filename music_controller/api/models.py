2de2e6236d8b69e3f34a67522d9afa0b54ae0345
brodie-m <brodiedmcguire@gmail.com>
2021-11-02 17:12:48 +0000

    added API list view

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -7,7 +7,10 @@ def generate_unique_code():
     length = 6
     while True:
         code = ''.join(random.choices(string.ascii_uppercase,k=length))
-        
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    
+    return code
 
         # Create your models here.
 
